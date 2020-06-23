537372c5c1db73d7b9a0fbc963650c3130fd741e
Joachim Defour <joachim.defour@icloud.com>
2020-06-24 01:11:11 +0200

    add add todo functionality, add App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
+import AddTodo from './components/AddTodo'
+
+import './App.css';
 
 class App extends Component {
   state = {
@@ -38,11 +41,24 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id )] })
   }
 
+  //add todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: 4,
+      title,
+      completed: false
+    }
+    this.setState({ todos: [...this.state.todos, newTodo] })
+  }
+
   render(){
     return (
       <div className="App">
-        <Header />
-        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} deleteTodo={this.deleteTodo} />
+        <div className="container">
+          <Header />
+          <AddTodo addTodo={this.addTodo} />
+          <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} deleteTodo={this.deleteTodo} />
+        </div>
       </div>
     );
   }
