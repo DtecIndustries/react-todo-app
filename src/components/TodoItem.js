d3442c4e5d0a2f35ded141b1902424bb8210fc74
Joachim Defour <joachim.defour@icloud.com>
2020-06-24 01:58:11 +0200

    minor, update propTypes

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -29,7 +29,9 @@ export class TodoItem extends Component {
 
 // propTypes
 TodoItem.propTypes = {
-  todo: PropTypes.object.isRequired
+  todo: PropTypes.object.isRequired,
+  toggleComplete: PropTypes.func.isRequired,
+  deleteTodo: PropTypes.func.isRequired
 }
 
 //style button
