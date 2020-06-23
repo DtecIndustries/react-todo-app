3a875f75d2fdb2e35ac992da3ecfc5fa2f6a9fb9
Joachim Defour <joachim.defour@icloud.com>
2020-06-24 01:16:26 +0200

    use uuid to generate todo id

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Header from './components/layout/Header'
 import Todos from './components/Todos'
 import AddTodo from './components/AddTodo'
+import {v4 as uuid} from 'uuid';
 
 import './App.css';
 
@@ -9,17 +10,17 @@ class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid(),
         title: 'Take out the trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid(),
         title: 'Dinner with friends',
         completed: true
       },
       {
-        id: 3,
+        id: uuid(),
         title: 'Meeting with boss',
         completed: false
       }
@@ -44,7 +45,7 @@ class App extends Component {
   //add todo
   addTodo = (title) => {
     const newTodo = {
-      id: 4,
+      id: uuid(),
       title,
       completed: false
     }
